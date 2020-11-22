ad36ab47a6235d387d138c75fd0a1ed704c4ddf0
maucosc <marc.sc@outlook.es>
2020-11-22 14:47:29 -0300

    inicio de proyecto, Creando archivo python

---
+++ main.py
@@ -0,0 +1,25 @@
+import mysql.connector
+db = mysql.connector.connect(host="localhost",
+                             user="root",
+                             passwd="",
+                             database="descarga_peliculas")
+
+def actualizar():
+    pass
+
+def espacios():
+    pass
+
+def borrar():
+    pass
+
+def CrearCuenta():
+    pass
+
+def visualizar():
+    pass
+
+def Menu():
+    pass
+
+
