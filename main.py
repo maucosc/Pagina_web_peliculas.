9750eb45117e9403f14b5cf68b2470aed9cb4daa
maucosc <marc.sc@outlook.es>
2020-12-02 00:33:16 -0300

    Creacion de archivos de crud separados de main

--- main.py
+++ main.py
@@ -1,100 +1,8 @@
-from crud import *
 from conexion import *
-
-
-def espacios():
-    espacio = 0
-    while (espacio <= 5):
-        espacio += 1
-        print("")
-
-
-def crearCuenta():
-    print("")
-    cursor = db.cursor()
-    nickname = input("Ingrese el nombre de usuario: ")
-    email = input("ingrese un correo: ")
-    passwd = input("Ingrese su contraseña: ")
-    cursor.execute(
-        "INSERT INTO cuenta (nickname,email,passwd) VALUES ('{}','{}',SHA2('{}',0));".format(nickname, email, passwd))
-    db.commit()
 
 
 def crear():
     pass
-
-
-def ingresarPelicula():
-    pass
-
-
-def ingresarFormato():
-    pass
-
-
-def ingresarServidor():
-    pass
-
-
-def insertar():
-    print("")
-    while (True):
-        print("----------")
-        print("Bienvenido")
-        print("----------")
-        print("")
-        print("A) Ingresar pelicula")
-        print("B) Ingresar Servidor")
-        print("C) Ingresar formato a pelicula")
-        print("D) Salir")
-        print("")
-
-        op1 = input("Ingrese su respuesta:")
-        op1 = op1.strip().lower()
-
-        if (op1 == "c" or op1 == "salir"):
-            espacios()
-            print("-------------------")
-            print("Adios vuelva pronto")
-            print("-------------------")
-            menuSesion()
-        elif (op1 == "a"):
-            ingresarPelicula()
-        elif (op1 == "b"):
-            ingresarServidor()
-        elif (op1 == "a"):
-            ingresarFormato()
-        elif (op1 != "a" or op1 != "b" or op1 != "c" or op1 != "d"):
-            print("---------------")
-            print("Opcion invalida")
-            print("---------------")
-            insertar()
-
-def visualizarPelicula():
-    while (True):
-        nombre = input("Buscar Pelicula")
-        cursor = db.cursor()
-        cursor.execute("SELECT pelicula.id AS 'N°',"
-                       "pelicula.nombre,"
-                       "pelicula.fecha,"
-                       "pelicula.resolucion,"
-                       "pelicula.idioma,"
-                       "pelicula.tamano AS 'Tamaño',"
-                       "categoria.nombre AS 'Categoria'"
-                       "pelicula.sinopsis"
-                       "FROM"
-                       "categoria"
-                       "INNERT JOIN categoria_pelicula ON categoria_pelicula.id_categoria_fk = categoria.id"
-                       "INNERT JOIN pelicula ON categoria_pelicula.id_pelicula_fk = pelicula.id"
-                       "WHERE pelicula.nombre LIKE '%{}%';".format(nombre))
-        rs = cursor.fetchall()
-        if(rs != rs):
-            print("")
-            print("Pelicula invalida")
-            print("")
-        else:
-            for x in rs:
-                print(x)
 
 
 def visualizar():
@@ -121,7 +29,6 @@ def iniciarSesion():
             while (True):
                 print("----")
                 print("Menu")
-                print("----")
                 print("")
                 print("A) Crear")
                 print("B) Visualizar Datos")
@@ -155,6 +62,7 @@ def iniciarSesion():
             print("---------------")
             print("Opcion invalida")
             print("---------------")
+            break
 
 
 def menuSesion():
